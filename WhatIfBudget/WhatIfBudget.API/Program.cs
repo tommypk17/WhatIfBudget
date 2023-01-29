4d3ed6fd93f833ad0ae7e5788a3d32c0406708a2
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-01-29 09:38:32 -0500

    #31 #67 add API base packages for auth & swagger

--- WhatIfBudget/WhatIfBudget.API/Program.cs
+++ WhatIfBudget/WhatIfBudget.API/Program.cs
@@ -1,17 +1,61 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+//Setup MS AD B2C Auth
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(options =>
+                {
+                    builder.Configuration.Bind("AzureAd", options);
+
+                    options.TokenValidationParameters.NameClaimType = "name";
+                },
+                    options => { builder.Configuration.Bind("AzureAd", options); });
+
+builder.Services.AddSwaggerGen();
+
+//Setup App CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowWebApp",
+        policy =>
+            policy.AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .WithOrigins(builder.Configuration.GetSection("CORS:allowed").Get<string[]>())
+    );
+});
+
+//Build the App
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseDeveloperExceptionPage();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseCors("AllowWebApp");
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.Run();
