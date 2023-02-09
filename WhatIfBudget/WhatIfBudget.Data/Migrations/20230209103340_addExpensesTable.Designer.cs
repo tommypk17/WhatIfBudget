1deaec4e7a361bb8ca5858ec7414ed2254c4c783
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-02-09 05:36:09 -0500

    #11 #66 add expenses table & migration

--- WhatIfBudget/WhatIfBudget.Data/Migrations/20230209103340_addExpensesTable.Designer.cs
+++ WhatIfBudget/WhatIfBudget.Data/Migrations/20230209103340_addExpensesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhatIfBudget.Data.DAL;
 
@@ -11,9 +12,10 @@ using WhatIfBudget.Data.DAL;
 namespace WhatIfBudget.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230209103340_addExpensesTable")]
+    partial class addExpensesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
