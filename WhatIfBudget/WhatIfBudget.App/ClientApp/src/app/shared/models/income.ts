93510a62d37e208ac9aefb46fd04e358c222445a
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-02-11 13:27:36 -0500

    #11 add name field to expense entry & table

--- WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/shared/models/income.ts
+++ WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/shared/models/income.ts
@@ -2,6 +2,7 @@ import { EFrequency } from "../enums/efrequency";
 
 export class Income {
   id: number | undefined;
+  name: string | undefined;
   amount: number | undefined;
   frequency: EFrequency | undefined;
 }
