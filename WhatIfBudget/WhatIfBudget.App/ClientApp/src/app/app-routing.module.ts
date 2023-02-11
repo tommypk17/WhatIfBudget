33ba7d880eb52447e97f34dddb2e532ee6beb31d
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-02-10 21:03:48 -0500

    add guard to all routes to force login

--- WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/app-routing.module.ts
+++ WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
-  { path: 'incomes', loadChildren: () => import('./modules/income/income.module').then(m => m.IncomeModule) },
-  { path: 'expenses', loadChildren: () => import('./modules/expense/expense.module').then(m => m.ExpenseModule) },
-  { path: 'goals', loadChildren: () => import('./modules/goal/goal.module').then(m => m.GoalModule) },
+  { path: 'incomes', loadChildren: () => import('./modules/income/income.module').then(m => m.IncomeModule), canActivate: [LoginGuard] },
+  { path: 'expenses', loadChildren: () => import('./modules/expense/expense.module').then(m => m.ExpenseModule), canActivate: [LoginGuard] },
+  { path: 'goals', loadChildren: () => import('./modules/goal/goal.module').then(m => m.GoalModule), canActivate: [LoginGuard] },
 ];
 
 @NgModule({
