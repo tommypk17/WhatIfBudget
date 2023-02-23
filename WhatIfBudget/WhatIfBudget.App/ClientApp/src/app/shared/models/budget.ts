605595e50d08b43edf6547a95e1d5e06adf6916b
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-02-23 18:20:03 -0500

    #7 #174 add budget service & getAllBudgets call

---
+++ WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/shared/models/budget.ts
@@ -0,0 +1,8 @@
+export class Budget {
+  id: number = 0;
+  name: string = '';
+  savingGoalId: number = 0;
+  debtGoalId: number = 0;
+  mortgageGoalId: number = 0;
+  investmentGoalId: number = 0;
+}
