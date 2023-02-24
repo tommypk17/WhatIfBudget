e3a71be07c9e37b8efb3759dfd86b2e9c1f8a65f
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-02-23 19:57:18 -0500

    #7 #174 when budget is selected, save in user's browser for use later.

--- WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/shared/budget-listing/budget-listing.component.ts
+++ WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/shared/budget-listing/budget-listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { BudgetService } from '../../services/budget.service';
+import { SharedService } from '../../services/shared.service';
 import { Budget } from '../models/budget';
 
 @Component({
@@ -12,7 +13,7 @@ export class BudgetListingComponent implements OnInit {
   private _budgets: Budget[] = [];
   selected: Budget | undefined;
 
-  constructor(private budgetService: BudgetService, private ref: DynamicDialogRef) { }
+  constructor(private sharedService: SharedService, private budgetService: BudgetService, private ref: DynamicDialogRef) { }
 
   ngOnInit(): void {
     this.budgetService.getBudgets().subscribe((res: Budget[]) => {
@@ -25,6 +26,9 @@ export class BudgetListingComponent implements OnInit {
   }
 
   loadBudget(): void {
-    this.ref.close(this.selected);
+    if (this.selected) {
+      this.sharedService.budget = this.selected.id;
+      this.ref.close(this.selected);
+    }
   }
 }
