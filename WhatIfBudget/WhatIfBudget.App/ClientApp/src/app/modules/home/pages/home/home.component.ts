8c9221702c8fd16e744b7fd108be3fa1f8f29997
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-03-03 18:19:44 -0500

    add expense, income, and net to homepage

--- WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/modules/home/pages/home/home.component.ts
+++ WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/modules/home/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpenseService } from '../../../../services/expense.service';
+import { IncomeService } from '../../../../services/income.service';
 import { SharedService } from '../../../../services/shared.service';
 import { Budget } from '../../../../shared/models/budget';
 
@@ -8,15 +10,27 @@ import { Budget } from '../../../../shared/models/budget';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  myMonthlyIncome: number = 0;
+  myMonthlyExpenses: number = 0;
 
   private _budget: Budget | undefined = this.sharedService.budget;
 
-  constructor(private sharedService: SharedService) { }
+  constructor(private sharedService: SharedService, private incomeService: IncomeService, private expenseService: ExpenseService) { }
 
   ngOnInit(): void {
     this.sharedService.budgetLoadedEmit.subscribe(() => {
       if (this.sharedService.budgetLoaded) this._budget = this.sharedService.budget;
     })
+    if (this.sharedService.budget.id) {
+      this.incomeService.getMonthlyIncomeByBudgetId(this.sharedService.budget.id).subscribe((res: number) => {
+        if (res) this.myMonthlyIncome = res;
+      });
+    }
+    if (this.sharedService.budget.id) {
+      this.expenseService.getMonthlyExpenseByBudgetId(this.sharedService.budget.id).subscribe((res: number) => {
+        if (res) this.myMonthlyExpenses = res;
+      });
+    }
   }
 
   get budget(): Budget | undefined {
