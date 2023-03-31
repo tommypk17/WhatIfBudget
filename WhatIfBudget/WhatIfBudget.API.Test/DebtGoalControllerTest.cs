141f4a14e0c92ddbd10d2570a1ac9e3804a4dead
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-03-31 06:17:05 -0400

    #22 #26 implement API test for debt goal balance overtime & totals

--- WhatIfBudget/WhatIfBudget.API.Test/DebtGoalControllerTest.cs
+++ WhatIfBudget/WhatIfBudget.API.Test/DebtGoalControllerTest.cs
@@ -95,6 +95,107 @@ namespace WhatIfBudget.API.Test
             actual.Should().BeEquivalentTo(expected);
         }
 
+        [TestMethod]
+        public void Get_DebtBalanceOverTime()
+        {
+            //mock income logic
+            var mockDGL = new Mock<IDebtGoalLogic>();
+            mockDGL.Setup(x => x.GetBalanceOverTime(It.IsAny<int>())).Returns
+                (new Dictionary<int, double>()
+                {
+                    { 0,100},
+                    { 1,90.08},
+                    { 2,80.15},
+                    { 3,70.21},
+                    { 4,60.26},
+                    { 5,50.3},
+                    { 6,40.33},
+                    { 7,30.36},
+                    { 8,20.38},
+                    { 9,10.39},
+                    { 10,0.39},
+                    { 11,0}
+                });
+
+            
+
+            //Setup the http context (for auth)
+            var debtController = new DebtGoalsController(mockDGL.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = Helper_MockHttpContext().Object
+                }
+            };
+
+            var expectedValue = new Dictionary<int, double>()
+                {
+                    { 0,100},
+                    { 1,90.08},
+                    { 2,80.15},
+                    { 3,70.21},
+                    { 4,60.26},
+                    { 5,50.3},
+                    { 6,40.33},
+                    { 7,30.36},
+                    { 8,20.38},
+                    { 9,10.39},
+                    { 10,0.39},
+                    { 11,0}
+                };
+
+            var expected = new ObjectResult(expectedValue)
+            {
+                StatusCode = 200,
+            };
+
+            var actual = debtController.GetBalanceOverTime(1);
+
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void Get_DebtTotals()
+        {
+            //mock income logic
+            var mockDGL = new Mock<IDebtGoalLogic>();
+            mockDGL.Setup(x => x.GetDebtTotals(It.IsAny<int>())).Returns
+                (new DebtGoalTotals()
+                {
+                    AllocationSavings = 0,
+                    MonthsToPayoff = 0,
+                    TotalCostToPayoff = 0,
+                    TotalInterestAccrued = 500000,
+                });
+
+            //Setup the http context (for auth)
+            var debtController = new DebtGoalsController(mockDGL.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = Helper_MockHttpContext().Object
+                }
+            };
+
+            var expectedValue = new DebtGoalTotals()
+            {
+                AllocationSavings = 0,
+                MonthsToPayoff = 0,
+                TotalCostToPayoff = 0,
+                TotalInterestAccrued = 500000,
+            };
+
+            var expected = new ObjectResult(expectedValue)
+            {
+                StatusCode = 200,
+            };
+
+            var actual = debtController.GetDebtTotals(1);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
         public Mock<HttpContext> Helper_MockHttpContext()
         {
             //Mock the principal & http context to pass in a fake user
