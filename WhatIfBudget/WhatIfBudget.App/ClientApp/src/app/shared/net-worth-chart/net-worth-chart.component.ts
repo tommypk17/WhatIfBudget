c653960893897d1b7b1711e32826ac3d578c4687
JetstreamDev <tmquint07@gmail.com>
2023-04-13 22:22:38 -0500

    #24 #277 Implement annotation lines for payoff dates in networth chart

--- WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/shared/net-worth-chart/net-worth-chart.component.ts
+++ WhatIfBudget/WhatIfBudget.App/ClientApp/src/app/shared/net-worth-chart/net-worth-chart.component.ts
@@ -1,8 +1,9 @@
-import { KeyValue } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { BudgetService } from '../../services/budget.service';
 import { SharedService } from '../../services/shared.service';
 import { NetValue } from '../models/budget';
+import { default as Annotation } from 'chartjs-plugin-annotation';
+require('chartjs-plugin-annotation');
 
 @Component({
   selector: 'app-net-worth-chart',
@@ -11,6 +12,7 @@ import { NetValue } from '../models/budget';
 })
 export class NetWorthChartComponent implements OnInit {
   basicData: any;
+  basicOptions: any;
 
   constructor(private budgetService: BudgetService, private sharedService: SharedService) { }
 
@@ -21,9 +23,16 @@ export class NetWorthChartComponent implements OnInit {
   loadChartInfo(): void {
     let months: string[] = [];
     let balances: number[] = [];
+    let debtGoalMonth: number | undefined;
+    let mortgageGoalMonth: number | undefined;
+    let savingGoalMonth: number | undefined;
 
     this.budgetService.getNetWorthOverTime(this.sharedService.budget.id!).subscribe((res: NetValue) => {
       if (res) {
+        debtGoalMonth = res.debtGoalMonth! / 12;
+        mortgageGoalMonth = res.mortgageGoalMonth! / 12;
+        savingGoalMonth = res.savingGoalMonth! / 12;
+
         if (res.balance.length > 12) {
           res.balance.forEach((v, i) => {
             if (v.key % 12 == 0) {
@@ -49,14 +58,59 @@ export class NetWorthChartComponent implements OnInit {
           labels: months,
           datasets: [
             {
+              label: 'Debt Payoff',
+              borderColor: 'red',
+              tension: .4
+            },
+            {
+              label: 'Mortgage Payoff',
+              borderColor: 'blue',
+              tension: .4
+            },
+            {
+              label: 'Savings Finish',
+              borderColor: 'green',
+              tension: .4
+            },
+            {
               label: 'Balance',
               data: balances,
               fill: false,
-              borderColor: 'red',
+              borderColor: 'black',
               tension: .4
-            }
+            },
           ]
         };
+
+        this.basicOptions = {
+          plugins: {
+            annotation: {
+              annotations: {
+                line: {
+                  type: 'line',
+                  xMin: debtGoalMonth,
+                  xMax: debtGoalMonth,
+                  borderColor: 'red',
+                  borderWidth: 1,
+                },
+                line1: {
+                  type: 'line',
+                  xMin: mortgageGoalMonth,
+                  xMax: mortgageGoalMonth,
+                  borderColor: 'blue',
+                  borderWidth: 1,
+                },
+                line2: {
+                  type: 'line',
+                  xMin: savingGoalMonth,
+                  xMax: savingGoalMonth,
+                  borderColor: 'green',
+                  borderWidth: 1,
+                },
+              }
+            }
+          }
+        }
       }
     });
   }
